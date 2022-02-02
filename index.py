c26d502f844efbd6522d62f584ff665e2a1c95ce
Your Name <alexsigala87@gmail.com>
2022-02-02 13:12:17 -0800

    practice qcreating and reading a CSV file with py

---
+++ index.py
@@ -0,0 +1,10 @@
+# create the csv
+compendium = open("compedium.csv","a")
+
+# add the content
+compendium.write("heres is some content")
+# 
+compendium.close()
+
+compendium.open("compendium.csv","r")
+print(compendium.read())
